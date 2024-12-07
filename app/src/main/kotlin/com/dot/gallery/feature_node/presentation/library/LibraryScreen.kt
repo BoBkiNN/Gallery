17014f339a1dad73c02baaa0040f1a62639f72e1
BoBkiNN <bobkin.yangirov@gmail.com>
2024-12-07 19:08:16 +0500

    replace single-item dropdown with settings icon

--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/library/LibraryScreen.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/library/LibraryScreen.kt
@@ -14,21 +14,16 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.VisibilityOff
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -78,23 +73,10 @@ fun LibraryScreen(
                 isScrolling = isScrolling,
                 activeState = searchBarActive
             ) {
-                var expandedDropdown by remember { mutableStateOf(false) }
-                IconButton(onClick = { expandedDropdown = !expandedDropdown }) {
+                IconButton(onClick = { navigate(Screen.SettingsScreen.route) }) {
                     Icon(
-                        imageVector = Icons.Outlined.MoreVert,
-                        contentDescription = stringResource(R.string.drop_down_cd)
-                    )
-                }
-                DropdownMenu(
-                    expanded = expandedDropdown,
-                    onDismissRequest = { expandedDropdown = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text(text = stringResource(id = R.string.settings_title)) },
-                        onClick = {
-                            expandedDropdown = false
-                            navigate(Screen.SettingsScreen.route)
-                        }
+                        imageVector = Icons.Outlined.Settings,
+                        contentDescription = stringResource(R.string.settings_title)
                     )
                 }
             }
